0d5d7337e23b1ce75a478e94b13df64f28aa191e
KeatonWakefield001 <keatonwakefield@csus.edu>
2021-11-19 00:18:58 -0800

    added the ability to register as a user and updated navigation

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ app_name = 'users'
 urlpatterns = [
     # Login page
     url('login/', LoginView.as_view(template_name= 'users/login.html'),name='login'),
+    # Registration page
+    url(r'^register/$', views.register, name='register'),
 ]
