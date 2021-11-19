982365efe20929f8ae22f98ce52a4f126463a67d
Thanh Le <thanhwaslike@gmail.com>
2021-11-19 02:01:03 -0800

    Implemented registering new user

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,8 @@ app_name = 'users'
 urlpatterns = [
     # Login page
     url('login/', LoginView.as_view(template_name= 'users/login.html'),name='login'),
+    # Logout page
     url('logout/', views.logout_view, name='logout'),
+    # Registration page
+    url('^register/', views.register, name='register'),
 ]
